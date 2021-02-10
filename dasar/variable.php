27050edc5ee8c08bb4a8e747f2622d1b5098703e
DisiKharismaP <disi.kharisma@gmail.com>
2021-02-10 10:26:37 +0700

    up

--- dasar/variable.php
+++ dasar/variable.php
@@ -2,10 +2,17 @@
 
 //variabel untuk menyimpan data
 //boleh terdapat angka, tapi tidak diawal
-$nama = "Bilqis";
+$nama = "Disi";
 
 echo $nama;
 echo"<p>";
 echo "Halo, nama saya $nama";
 
 ?>
+
+
+
+
+
+
+
